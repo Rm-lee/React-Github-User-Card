d31b55acc68ae10a53099ac1846c0b7022404d99
Roger Lee <rmlee0003@gmail.com>
2019-10-06 20:34:54 -0400

    created axios request in cdm, created UserCard component to display the user data

---
+++ r-g-u-c/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import UserCard from './components/UserCard'
+import axios from 'axios'
+class App extends Component {
+  constructor() {
+    super()
+    this.state={
+      user:'rmlee',
+      data:[]
+    }
+  }
+  componentDidMount() {
+    axios.get(`https://api.github.com/users/${this.state.user}`)
+    .then(result => {
+      this.setState({
+        data: result.data
+      })
+      console.log(result)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+
+  }
+  render() {
+    return (
+      <div>
+        <UserCard userData={this.state.data}/>
+      </div>
+    );
+  }
+}
+
+export default App;
